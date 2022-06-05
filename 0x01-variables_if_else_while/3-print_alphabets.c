df90111d5406401b62f7521076009f529c4d5adf
Shamara12 <shamarasolomon@gmail.com>
2022-06-06 00:17:27 +0100

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Displays letters of the alphabet a-z in lowercase, then in uppercase.
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	char letters[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(letters[i]);
+	}
+	putchar('n');
+	return (0);
+}
