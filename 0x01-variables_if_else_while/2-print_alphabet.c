336236b51f05fb93b6d071e54c81e7ed3b7e242f
Shamara12 <shamarasolomon@gmail.com>
2022-06-05 23:28:28 +0100

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Displays all the letters of the alphet in lowercase
+ *
+ *Return: Always 0 
+ */
+
+int main(void)
+{
+	char letters[26] = "abcdefghijklmnopqrstuvwxyz";
+	int = i;
+	for (i = 0; i < 26; i++;)
+	{
+		putchar(letters[i]);
+	}
+	putchar('\n');
+	return (0);
+}
