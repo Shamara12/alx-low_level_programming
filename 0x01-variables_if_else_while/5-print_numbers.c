2c1a92fab6e193d3504defb53c8e56a7564a579c
Shamara12 <shamarasolomon@gmail.com>
2022-06-07 23:29:20 +0100

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to 9.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+	{
+		char numbers[10] = "0123456789";
+		int i;
+
+		for (i = 0; i < 10; i++)
+		{
+			putchar(numbers[i]);
+		}
+		putchar('\n');
+		return (0);
+	}
