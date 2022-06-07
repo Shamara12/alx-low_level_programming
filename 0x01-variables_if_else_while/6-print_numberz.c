14bc6d15f42b679dfe4d6ab4268401b2e2cf9d09
Shamara12 <shamarasolomon@gmail.com>
2022-06-08 00:09:49 +0100

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers 0-9.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+	{
+		int i;
+
+		for (i = 48; i < 58; i++)
+		{
+			putchar(i);
+		} 
+		putchar('\n');
+		return (0);
+	}
