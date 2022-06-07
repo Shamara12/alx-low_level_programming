ccb89d5d9cf2be6c48087f1158e88d7662d64ddc
Shamara12 <shamarasolomon@gmail.com>
2022-06-07 23:11:32 +0100

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the letters of the alphabet except 'q' and 'e'.
+ *
+ * Always Return 0.
+ */
+
+int main(void)
+	{
+	char letters[24] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+	
+	for (i = 0; i < 24; i++)
+	{
+		putchar(letters[i]);
+	}
+	putchar('\n');
+	return (0);
+	}
