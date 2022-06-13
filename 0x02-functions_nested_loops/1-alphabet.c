9496bbfd0defc347637d313f83c8adae2d01c8d9
Shamara12 <shamarasolomon@gmail.com>
2022-06-13 01:43:13 +0100

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,13 @@
  */
 
 void print_alphabet(void);
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
 	{
-		int i;
-		for (i = 97; i < 123; i++)
-		{
-			putchar(i);
-		}
-		putchar('\n');
-		return (0);
+		putchar(i);
 	}
+	putchar('\n');
+	return (0);
+}
