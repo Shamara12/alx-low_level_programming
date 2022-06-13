3377d380a90ffd52cc2fbc2dfd03b0afa2642a76
Shamara12 <shamarasolomon@gmail.com>
2022-06-13 01:40:22 +0100

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints all the letters of the alphabet in lowercase.
+ * Return: Always 0.
+ */
+
+void print_alphabet(void);
+	{
+		int i;
+		for (i = 97; i < 123; i++)
+		{
+			putchar(i);
+		}
+		putchar('\n');
+		return (0);
+	}
