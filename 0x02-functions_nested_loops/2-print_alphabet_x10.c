cff50bc89d7431868058c53dd66fcdc3d837c454
Shamara12 <shamarasolomon@gmail.com>
2022-06-13 02:04:36 +0100

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * void print_alphabet_x10 - prints lowercse letters of the alphabet, 10 times.
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int x, y;
+
+	for(x = 0; x < 10; x++)
+	{
+		for(y = 97; y < 123; y++)
+		{
+			putchar(y);
+		}
+	}
+	putchar('\n');
+}
