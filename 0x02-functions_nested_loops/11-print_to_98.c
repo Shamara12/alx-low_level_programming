ebafb0c22844e76e097b142b85604d0f644bada1
Shamara12 <shamarasolomon@gmail.com>
2022-06-20 04:38:21 +0100

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints from entered number to 98. *
+ * @n: inputed integer.
+ *
+ * Return: no return
+ */
+
+void print_to_98(int n)
+{
+	if (n < 99)
+	{
+		for (; n < 99; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n > 98)
+	{
+		for (; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	printf("%d\n", n);
+	}
+}
