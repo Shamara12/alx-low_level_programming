31527555560b57d57850f63a409375fd41347f30
Shamara12 <shamarasolomon@gmail.com>
2022-06-22 22:27:54 +0100

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/** 
+ * print_numbers - prints numbers from 0 - 9.
+ *
+ * Return: No return.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
