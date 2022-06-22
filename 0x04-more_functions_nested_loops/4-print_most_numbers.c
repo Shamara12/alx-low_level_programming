4fd60ece8eb65156eaac9b96dd1dd16a54e4186b
Shamara12 <shamarasolomon@gmail.com>
2022-06-22 22:51:30 +0100

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0-9 except 2 and 4.
+ *
+ * Return: no return.
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 && i != 52)
+			putchar(i);
+	}
+	putchar('\n');
+}
