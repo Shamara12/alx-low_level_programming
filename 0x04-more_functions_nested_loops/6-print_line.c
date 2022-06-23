c5593296e25a11dd20a5850163141191838b6190
Shamara12 <shamarasolomon@gmail.com>
2022-06-23 19:41:43 +0100

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,9 +12,12 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
+		putchar('\n');
+	else
 	{
-		putchar(95);
+		for (i = 1; i <= n; ++i)
+			putchar(95);
+		putchar('\n');
 	}
-	putchar('\n');
 }
