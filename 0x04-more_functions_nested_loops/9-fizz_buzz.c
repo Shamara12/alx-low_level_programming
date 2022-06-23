51c179f0fc56bf8fde60178a4888d22cc65eb120
Shamara12 <shamarasolomon@gmail.com>
2022-06-23 22:49:14 +0100

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,19 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
-                {
-                        printf("FizzBuzz\t");
+		{
+			printf(" FizzBuzz");
                 }
 		else if (i % 3 == 0)
 		{
-			printf("Fizz\t");
+			printf(" Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz\t");
+			printf(" Buzz");
 		}
 		else
-			printf("%d\t", i);
+			printf(" %d", i);
 	}
 	putchar('\n');
 	return (0);
