45f79f2b5a8a28b263640ffc8fb3314abe2d9337
Shamara12 <shamarasolomon@gmail.com>
2022-06-23 04:43:21 +0100

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0-14, 10 times.
+ *
+ * Return: no return.
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y >= 10)
+			
+				putchar((y / 10) + 48);
+			putchar((y % 10) + 48);
+		}
+		putchar('\n');
+	}
+}
