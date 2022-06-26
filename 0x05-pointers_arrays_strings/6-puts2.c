b71cca393ac3f5f4c54dcea16773dc2e8aab811d
Shamara12 <shamarasolomon@gmail.com>
2022-06-26 21:28:43 +0100

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _puts2 - prints each character of a string on a new line.
+ *
+ * str: inputed string.
+ *
+ * Return: no return.
+ */
+
+void puts2(char *str)
+{
+	int s = 0;
+
+	while (s >= 0)
+	{
+		if (str[s] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[s]);
+		s++;
+		_putchar('\n');
+	}
+}
